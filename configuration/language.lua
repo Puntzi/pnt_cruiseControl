d7d872f427d04b79cdd9621fe67527f7eeef6112
Puntzi <70822492+Puntzi@users.noreply.github.com>
2022-04-09 20:39:44 +0200

    Update language.lua

--- configuration/language.lua
+++ configuration/language.lua
@@ -2,6 +2,6 @@ Strings = {
     ["args_invalid"]    = "The arguments need to be a number",
     ["not_in_vehicle"]  = "You need to be in a vehicle",
     ["not_driver_seat"] = "You need to be in driver seat",
-    ["cruiser_set_at"]  = "Cruiser setted the cruiser at ~g~%s~s~ km/h",
+    ["cruiser_set_at"]  = "Cruiser setted the cruiser at ~g~%s~s~ %s",
     ["no_cruiser"]      = "Cruiser desactivated"
 }
